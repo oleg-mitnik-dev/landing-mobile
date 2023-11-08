64d7aced985942f7c1db2e40b624b270f3a9b9ee
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-11-08 12:09:23 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// npx tailwindcss -i ./assets/styles/input.css -o ./assets/styles/output.css --watch
