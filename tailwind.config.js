c52435a49304d5ebf3888fdafc9a559b87382314
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-11-08 18:05:21 +0300

    Mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Roboto"', ...defaultTheme.fontFamily.sans],
+        'roboto': '"Roboto", sans-serif'
+      },
+    },
   },
   plugins: [],
 };
 
 // npx tailwindcss -i ./assets/styles/input.css -o ./assets/styles/output.css --watch
+// San Francisco Pro Text (SF Pro Text) --  для текста до 19 включительно
+// San Francisco Pro Display (SF Pro Display) -- для текста от 20 и выше
